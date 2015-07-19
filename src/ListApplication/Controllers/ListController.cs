b20c48cb242a84a052381f5bbacac1ff195d8951
Lougenia Bailey <lougenia.bailey@excella.com>
2015-07-19 17:13:26 -0400

    Added Post method, changed listItems from IEnumerable to ICollection, updated commented REST calls.

--- src/ListApplication/Controllers/ListController.cs
+++ src/ListApplication/Controllers/ListController.cs
@@ -1,47 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-
-// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ListApplication.Controllers
 {
     [Route("api/[controller]")]
     public class ListController : Controller
     {
-        IEnumerable<ListItem> listItems = new List<ListItem> {
+        ICollection<ListItem> listItems = new List<ListItem> {
             new ListItem { Id = 1, Name = "My Item", IsActive = true }
         };
         
-        // GET: api/values
+        // GET: api/list
         [HttpGet(Name ="getAll")]
         public IEnumerable<ListItem> Get()
         {
-            return listItems;
+            return this.listItems;
         }
 
-        // GET api/values/5
+        // GET api/list/5
         [HttpGet("{id}", Name = "getById")]
         public ListItem Get(int id)
         {
-            return listItems.FirstOrDefault(listItem => listItem.Id == id);
+            return this.listItems.FirstOrDefault(listItem => listItem.Id == id);
         }
 
-        //// POST api/values
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-        //}
+        // POST api/list
+        [HttpPost(Name = "create")]
+        public IEnumerable<ListItem> Post([FromBody]string value)
+        {
+            var id = (this.listItems.Max(listItem => (int?)listItem.Id) ?? 0) + 1;
+            this.listItems.Add(new ListItem() { Id = id, Name = value, IsActive = true });
 
-        //// PUT api/values/5
+            return this.listItems;
+        }
+
+        //// PUT api/list/5
         //[HttpPut("{id}")]
         //public void Put(int id, [FromBody]string value)
         //{
         //}
 
-        //// DELETE api/values/5
+        //// DELETE api/list/5
         //[HttpDelete("{id}")]
         //public void Delete(int id)
         //{
